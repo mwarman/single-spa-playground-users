1aec577ac3a6edf575d7d9da0f7a8f91e5eeecb1
Matthew Warman <warmanmr@gmail.com>
2024-06-13 07:51:00 -0400

    lint fixes

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,9 +1,11 @@
-import { render } from "@testing-library/react";
-import Root from "./root.component";
+import React from 'react';
+import { render } from '@testing-library/react';
 
-describe("Root component", () => {
-  it("should be in the document", () => {
+import Root from './root.component';
+
+describe('Root component', () => {
+  it('should be in the document', () => {
     const { getByText } = render(<Root name="Testapp" />);
-    expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
+    expect(getByText(/Testapp is mounted!/i)).toBeDefined();
   });
 });
