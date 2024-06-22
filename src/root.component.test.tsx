46a4d634cd8732a7d7dffbab5000a49f72e9b18b
Matt Warman <mwarman@users.noreply.github.com>
2024-06-22 07:11:12 -0400

    Upgrade dependencies (#1)
    * node version config
    
    * prettier config
    
    * deps
    
    * lint fixes
    
    * test config
    
    * docs
    
    * ci workflow
    
    * deps
    
    * deps
    
    * deps

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -1,9 +1,15 @@
-import { render } from "@testing-library/react";
-import Root from "./root.component";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 
-describe("Root component", () => {
-  it("should be in the document", () => {
-    const { getByText } = render(<Root name="Testapp" />);
-    expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
+import Root from './root.component';
+
+describe('Root component', () => {
+  it('should be in the document', async () => {
+    // ARRANGE
+    render(<Root name="Testapp" />);
+    await screen.findByTestId('root');
+
+    // ASSERT
+    expect(screen.getByText(/Testapp is mounted!/i)).toBeDefined();
   });
 });
